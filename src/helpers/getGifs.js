68ea0601457cf861088636de8e40277ced4cfade
Natalia Caballero <nataliac.desarrollopersonal@gmail.com>
2024-02-05 15:55:01 -0500

    First commit

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,15 @@
+export const getGifs = async (category) => {
+    const api_key = "PoNdwmLg4BRDTHdFdCfCjucRG57oZvQD";
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=${api_key}&q=${category}&limit=10`;
+
+    const res = await fetch(url);
+    const {data = []} = await res.json();
+
+    const gifs = data.map(img => ({
+        id: img.id,
+        title: img.title,
+        url: img.images.downsized_medium.url
+    }));
+    return gifs;
+
+}
